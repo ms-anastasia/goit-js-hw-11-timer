9dee4b0f5ec0d7755469be8e72dd1e14b761d046
ms-anastasia <nastyakorn0@gmail.com>
2021-10-24 21:04:39 +0300

    adds coundowner

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.init();
+  
+   
+    this.days = document.querySelector('[data-value="days"]');
+    this.hours = document.querySelector('[data-value="hours"]');
+    this.mins = document.querySelector('[data-value="mins"]');
+    this.secs = document.querySelector('[data-value="secs"]');
+  }
+  init() {
+    setInterval(() => {
+      const currentTime = Date.now();
+      const time = this.targetDate - currentTime;
+      this.changeTimer(this.getTime(time));
+    }, 1000);
+  }
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+  getTime(time) {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
 
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
 
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    return { days, hours, mins, secs }
+  }
+  changeTimer({ days, hours, mins, secs }) {
+    this.days.textContent = `${days}`;
+    this.hours.textContent = `${hours}`;
+    this.mins.textContent = `${mins}`;
+    this.secs.textContent = `${secs}`;
+  }
+}
 new CountdownTimer({
   selector: '#timer-1',
     targetDate: new Date('01/01/2022'),
   
 });
-const getTimeDif = () => {
-    const diff = targetDate - new Date();
-    const getDays = days(diff);
-    const getHours = hours(diff);
-    const getMins = mins(diff);
-    const getSrcs = secs(diff);
-}
+
